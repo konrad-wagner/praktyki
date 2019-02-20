4e7750df369d4095e5805ab0bc04136d4f300b4e
konrad-wagner <47723759+konrad-wagner@users.noreply.github.com>
2019-02-20 07:05:48 +0100

    Add files via upload

---
+++ www 19.02/scripts/main.js
@@ -0,0 +1,46 @@
+﻿var naglowek = document.querySelector('h1');
+naglowek.textContent= "Trochę o C#";
+
+
+
+document.querySelector('body').oncopy = function()
+{
+    alert("Nie kopiuj. Ja wszystko widzę ;)")
+}
+
+var zmianaObrazka = document.querySelector('img');
+zmianaObrazka.onclick = function()
+{
+    var zrodlo = zmianaObrazka.getAttribute('src');
+    if(zrodlo === 'images/csharp.png')
+        {
+            zmianaObrazka.setAttribute('src', 'images/zdj2.jpg');
+        }
+    else
+        {
+            zmianaObrazka.setAttribute('src', 'images/csharp.png');
+        }
+}
+
+var przycisk = document.querySelector('button');
+var naglowek = document.querySelector('h1');
+
+    
+    
+  function setUserName() 
+    {
+    var imie = prompt('Twoje imie:');
+    localStorage.setItem('imie', imie);
+    naglowek.textContent = 'Trochę o C# ' + imie;
+    }
+
+    przycisk.onclick = function() 
+    {
+    setUserName();
+    }
+
+
+
+
+
+
